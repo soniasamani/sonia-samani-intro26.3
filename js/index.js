49890d8f147b9f525d4a6fbe47188b939728f445
Sonia Samani <sonia.k.samani@gmail.com>
2026-09-01 20:02:16 -0400

    Lesson9-API fetch completed

--- js/index.js
+++ js/index.js
@@ -61,6 +61,33 @@ messageForm.addEventListener("submit", function(e) {
   e.target.reset();
 });
 
+fetch("https://api.github.com/users/soniasamani/repos")
+  .then(response => {
+     return response.json();
+  })
+  .then(data => {
+      const repositories = data;
+      console.log(repositories);
+      const projectSection = document.querySelector("#projects");
+      const projectList = projectSection.querySelector("ul");
+      for (let i = 0; i < repositories.length; i++) {
+        const project = document.createElement("li");
+      
+        const projectLink = document.createElement("a");
+        projectLink.innerText = repositories[i].name;
+        projectLink.href = repositories[i].html_url;
+        projectLink.target = "_blank";
+      
+        project.appendChild(projectLink);
+      
+        projectList.appendChild(project);
+      }
+  })
+  .catch(error => {
+      console.error(error);
+  });
+
+
 
 
 
